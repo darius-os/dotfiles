8d153a47d1fc562da4288dd1964f1f28941a30fc
Dory <do.kh@protonmail.com>
2020-05-15 20:49:35 +0300

    add commonly config files

---
+++ .doom.d/config.el
@@ -0,0 +1,12 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com"
+      projectile-project-search-path '("~/Dropbox/code/" "~/bin/"))
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+(setq doom-theme 'doom-one)
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
