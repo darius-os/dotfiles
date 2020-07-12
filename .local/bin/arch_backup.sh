8cb88c053bcb8dfc6db7b34a07efcd654678b2a9
darius-os <do.kh@protonmail.com>
2020-07-12 22:38:19 +0300

    (｡◕‿◕｡)

--- .local/bin/arch_backup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# do not use with zsh
-
-sudo rsync -aAXv --delete --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude="swapfile" --exclude="lost+found" --exclude=".cache" --exclude=/home/darius/.npm/* --exclude=/home/darius/.stack/* --exclude=".VirtualBoxVMs" --exclude=/home/darius/.local/share/venv/* --exclude="*.pyc" --exclude=".ecryptfs" / /mnt/backup/
-
